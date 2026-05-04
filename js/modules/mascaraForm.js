50b4ef43091c2646b9d75a26c0a8bc42dc77a14f
Paulo Henrique <pauloluz566@gmail.com>
2026-05-04 15:50:09 -0300

    fix(mascaraForm): Ajuste da mascara para validação do telefone e do nome

--- js/modules/mascaraForm.js
+++ js/modules/mascaraForm.js
@@ -5,4 +5,23 @@ export function initMascaraForm() {
   if (!campoTelefone) return;
 
   $('#telefone').mask('(00) 00000-0000');
+
+  const validar = () => {
+    const apenasDigitos = campoTelefone.value.replace(/\D/g, '');
+    if (apenasDigitos.length < 11) {
+      campoTelefone.setCustomValidity('Digite um telefone completo com DDD.');
+    } else {
+      campoTelefone.setCustomValidity('');
+    }
+  };
+
+  $('#telefone').on('input keyup paste', validar);
+
+  campoTelefone.closest('form')?.addEventListener('submit', (e) => {
+    validar();
+    if (!campoTelefone.checkValidity()) {
+      campoTelefone.reportValidity();
+      e.preventDefault();
+    }
+  });
 }
